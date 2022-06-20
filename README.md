a3b4b67776b1ae7fae52dfa08643e230ac7dc5b0
neven <nevendrean@yahoo.fr>
2022-06-20 10:03:49 -0500

    reworked app.js and Readme

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Authenticate a Rails API backend with JWT, with Authorization-bearer or HttpOnly Cookie
 
-A (very simple) Rails API with a (very simple) front-end  - powered by Parcel here - to interact with it. Two gems are used, **jwt** and **bcrypt**.
+A (very simple) Rails API with a (very simple) front-end powered by Parcel here to interact with it (or Postman).
+Two gems are used: **jwt** and **bcrypt**.
 Bcrypt is used for the password with `has_secure_password` on the model that filters any output and saves a crypted password in the database. It also gives the `user.authenticate(password)` method.
-The "jwt" produces JwtTokens from a secret salt (used Rails.secret_key here). It gives gives 2 methods JWT.encode and JWT.decode with specific errors.
+The "jwt" produces JwtTokens from a secret salt (used Rails.secret_key here). It gives gives 2 methods `JWT.encode` and `JWT.decode` adn renders **specific errors**.
+
+## Two branches, cookie and bearer
+
+Two versions: "main" with a cookie, "jwt-authenticate" with bearer.
+
+- "main" with HttpOnly cookie. Needs the header `credentials: 'include'` front-side,  and for the back-end, we need "cors" with "credentials: true" in the CORS intializer back-end and the midlleware "  include ActionController::Cookies". Except singup and login, every request includes the cookie that the back-end reads and decodes. The jwt gem returns the decoded token or "error" (expired of invalid).
+
+- "jwt-authenticate" with Authorization. The token is saved in **localStorage**. We only need to pass a header "Authorization: bearer -jwt_token-" on each request for authentication (except the signup and login). The jwt decodes or returns an error (expired or invalid).
+
+## Run
 
 Run in 2 terminals:
 
@@ -11,14 +22,14 @@ rails s
 parcel index.html
 ```
 
-Two versions: "main" with a cookie, "jwt-authenticate" with bearer.
+> !! RAILS does **not want headers: { "Content-type": "application/json"}** when **formdata** !!
 
-- "main" with HttpOnly cookie. Needs the header `credentials: 'include'` front-side,  and for the back-end, we need "cors" with "credentials: true" in the CORS intializer back-end and the midlleware "  include ActionController::Cookies". Except singup and login, every request includes the cookie that the back-end reads and decodes. The jwt gem returns the decoded token or "error" (expired of invalid).
+## Sources
 
-- "jwt-authenticate with Authorization. The token is saved in **localStorage**. We only need to pass a header "Authorization: bearer -jwt_token-" on each request for authentication (except the signup and login). The jwt decodes or returns an error (expired or invalid).
+[For cookie](https://www.thegreatcodeadventure.com/jwt-storage-in-rails-the-right-way/)
 
-[The source](https://www.thegreatcodeadventure.com/jwt-storage-in-rails-the-right-way/)
-
-[Csrf](https://blog.eq8.eu/article/rails-api-authentication-with-spa-csrf-tokens.html)
+[Note on Csrf](https://blog.eq8.eu/article/rails-api-authentication-with-spa-csrf-tokens.html)
 
 [Source](https://learn.co/lessons/jwt-auth-rails)
+
+[Notes on session in Rails](https://orbit.love/blog/managing-server-side-sessions-in-rails)
--- front/app.js
+++ front/app.js
@@ -1,4 +1,135 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = 'http://localhost:3000';
+
+// !! RAILS does not want headers: { "Content-type": "application/json"} when formdata !!
+
+// SIGNUP => POST
+const { signup } = document.forms;
+signup.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(signup);
+  [...formData.entries()].forEach((elt) => {
+    formData.append(elt[0], elt[1]);
+  });
+
+  try {
+    const req = await fetch(`${BASE_URL}/signup`, {
+      method: 'POST',
+      credentials: 'include',
+      body: formData,
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const res = await req.json();
+    if (!res.errors) {
+      console.log(res);
+      document.getElementById('jwt').innerHTML = JSON.stringify(res.jwt);
+    } else {
+      document.getElementById('signinErrors').innerHTML = JSON.stringify(
+        res.errors,
+      );
+    }
+    signup.reset();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// LOGIN => POST
+const { login } = document.forms;
+// const login = document.forms["login"];
+login.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(login);
+  [...formData.entries()].forEach((elt) => {
+    formData.append(elt[0], elt[1]);
+  });
+
+  try {
+    const req = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      credentials: 'include',
+      body: formData,
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const res = await req.json();
+    if (!res.errors) {
+      console.log(res);
+      document.getElementById('jwt').innerHTML = JSON.stringify(res.jwt);
+    } else {
+      document.getElementById('loginErrors').innerHTML = JSON.stringify(
+        res.errors,
+      );
+    }
+    login.reset();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const currentUser = document.getElementById('current');
+currentUser.addEventListener('click', async (e) => {
+  e.preventDefault();
+  try {
+    const req = await fetch(`${BASE_URL}/current`, {
+      credentials: 'include',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+    const resp = await req.json();
+    document.getElementById('current_user').innerHTML = JSON.stringify(resp);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const logout = document.getElementById('logout');
+logout.addEventListener('click', async (e) => {
+  e.preventDefault();
+  return fetch(`${BASE_URL}/logout`, {
+    credentials: 'include',
+    hearders: {
+      'Content-type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((msg) => {
+      document.getElementById('jwt').innerHTML = '';
+      document.getElementById('current_user').innerHTML = '';
+      alert(msg.message);
+    })
+    .catch((err) => console.error(err));
+});
+
+const users = document.getElementById('users');
+users.addEventListener('click', async (e) => {
+  e.preventDefault();
+  try {
+    const request = await fetch(`${BASE_URL}/users`, {
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+    if (request) {
+      const response = await request.json();
+      if (!response.error) {
+        document.getElementById('users_list').innerHTML = JSON.stringify(response);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // send the CRSF cookie for POST/PUT/PATCH requests
 // function getCSRFToken() {
@@ -7,136 +138,3 @@ const BASE_URL = "http://localhost:3000";
 //     .find((cookie) => cookie.startsWith("CSRF-TOKEN="))
 //     .split("=")[1];
 // }
-
-const currentUser = document.getElementById("current");
-currentUser.addEventListener("click", async (e) => {
-  try {
-    const req = await fetch(`${BASE_URL}/current`, {
-      method: "get",
-      credentials: "include",
-      headers: {
-        "Content-type": "application/json",
-        Accept: "application/json",
-      },
-    });
-    const resp = await req.json();
-    document.getElementById("current_user").innerHTML = JSON.stringify(resp);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-const logout = document.getElementById("logout");
-logout.addEventListener("click", async (e) => {
-  e.preventDefault();
-  return await fetch(`${BASE_URL}/logout`, {
-    method: "GET",
-    credentials: "include",
-    hearders: {
-      "Content-type": "application/json",
-      Accept: "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((msg) => {
-      document.getElementById("jwt").innerHTML = "";
-      document.getElementById("current_user").innerHTML = "";
-      console.log(msg);
-    })
-
-    .catch((err) => console.log(err));
-});
-
-const signup = document.forms["signup"];
-signup.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const formData = new FormData(signup);
-  [...formData.entries()].forEach((e) => {
-    formData.append(e[0], e[1]);
-  });
-
-  try {
-    const req = await fetch(`${BASE_URL}/signup`, {
-      method: "POST",
-      credentials: "include",
-      body: formData,
-      headers: {
-        Accept: "application/json",
-      },
-    });
-    const res = await req.json();
-    console.log(res.errors);
-    if (!res.errors) {
-      document.getElementById("jwt").innerHTML = JSON.stringify(res.jwt);
-    } else {
-      console.log(res.errors);
-      document.getElementById("signinErrors").innerHTML = JSON.stringify(
-        res.errors
-      );
-    }
-    console.log(res);
-    signup.reset();
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// !!!!! RAILS does not want headers: { "Content-type": "application/json"} when formdata !!!!!
-
-/// POST LOGIN
-const login = document.forms["login"];
-login.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const formData = new FormData(login);
-  [...formData.entries()].forEach((e) => {
-    formData.append(e[0], e[1]);
-  });
-
-  try {
-    const req = await fetch(`${BASE_URL}/login`, {
-      method: "POST",
-      credentials: "include",
-      body: formData,
-      headers: {
-        // "X-CSRF-Token": getCookie("CSRF-TOKEN"),
-        Accept: "application/json",
-      },
-    });
-    const res = await req.json();
-    if (!res.errors) {
-      document.getElementById("jwt").innerHTML = JSON.stringify(res.jwt);
-    } else {
-      console.log(res);
-    }
-    login.reset();
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-const users = document.getElementById("users");
-users.addEventListener("click", async (e) => {
-  e.preventDefault();
-  try {
-    const request = await fetch(`${BASE_URL}/users`, {
-      method: "GET",
-      mode: "cors",
-      credentials: "include",
-      headers: {
-        "Content-type": "application/json",
-        Accept: "application/json",
-      },
-    });
-    if (request) {
-      const response = await request.json();
-      if (!response.error) {
-        document.getElementById("users_list").innerHTML =
-          JSON.stringify(response);
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
